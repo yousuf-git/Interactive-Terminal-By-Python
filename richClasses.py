6ca733a2da0d3f0c71dab00a6cb09c780370d63b
yousuf-git <hackerbest052@gmail.com>
2024-07-25 19:33:45 +0500

    Added: List of important classes in this library

---
+++ richClasses.py
@@ -0,0 +1,15 @@
+# Below are the classes that are imported from the rich library
+
+from rich import print as rprint
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel
+from rich.syntax import Syntax
+from rich.markdown import Markdown
+from rich.progress import Progress
+from rich.live import Live
+from rich.text import Text
+from rich.columns import Columns
+from rich.box import Box
+from rich.rule import Rule
+from rich.panel import Panel
